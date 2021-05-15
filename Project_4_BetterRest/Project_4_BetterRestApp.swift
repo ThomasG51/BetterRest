4f6d7947ea659245f6c8d3573d78af3465525e61
Thomas <contact@thomas-george.com>
2021-05-15 17:34:55 +0200

    Initial Commit

---
+++ Project_4_BetterRest/Project_4_BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Project_4_BetterRestApp.swift
+//  Project_4_BetterRest
+//
+//  Created by Thomas George on 15/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct Project_4_BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
